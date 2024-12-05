6bd99db19243855275959c88e88236fee835c10e
Oscar Motto Varea <oscar.motto@mail.schwarz>
2024-12-05 09:56:18 +0100

    feat: Added more tests for input parsing on day05 problem

--- src/day05/main_test.go
+++ src/day05/main_test.go
@@ -1,10 +1,82 @@
 package main
 
 import (
+	"github.com/google/go-cmp/cmp/cmpopts"
 	"testing"
 
 	"github.com/google/go-cmp/cmp"
 )
+
+func TestParseInput(t *testing.T) {
+	tcs := []struct {
+		desc            string
+		input           []string
+		expectedUpdates [][]int
+		expectedRules   map[int][]int
+	}{
+		{
+			desc: "Parse input to get rules and updates",
+			input: []string{
+				"47|53",
+				"97|13",
+				"97|61",
+				"97|47",
+				"75|29",
+				"61|13",
+				"75|53",
+				"29|13",
+				"97|29",
+				"53|29",
+				"61|53",
+				"97|53",
+				"61|29",
+				"47|13",
+				"75|47",
+				"97|75",
+				"47|61",
+				"75|61",
+				"47|29",
+				"75|13",
+				"53|13",
+				"",
+				"75,47,61,53,29",
+				"97,61,53,29,13",
+				"75,29,13",
+				"75,97,47,61,53",
+				"61,13,29",
+				"97,13,75,29,47",
+			},
+			expectedUpdates: [][]int{
+				{75, 47, 61, 53, 29},
+				{97, 61, 53, 29, 13},
+				{75, 29, 13},
+				{75, 97, 47, 61, 53},
+				{61, 13, 29},
+				{97, 13, 75, 29, 47},
+			},
+			expectedRules: map[int][]int{
+				29: {13},
+				47: {53, 13, 61, 29},
+				53: {29, 13},
+				61: {13, 53, 29},
+				75: {29, 53, 47, 61, 13},
+				97: {13, 61, 47, 29, 53, 75},
+			},
+		},
+	}
+	for _, tc := range tcs {
+		t.Run(tc.desc, func(t *testing.T) {
+			rules, start := getMapOrderingRules(tc.input)
+			if diff := cmp.Diff(tc.expectedRules, rules, cmpopts.SortMaps(func(x, y int) bool { return x < y })); diff != "" {
+				t.Errorf("sum has diff %s", diff)
+			}
+			updates := getUpdates(tc.input, start+1)
+			if diff := cmp.Diff(tc.expectedUpdates, updates); diff != "" {
+				t.Errorf("sum has diff %s", diff)
+			}
+		})
+	}
+}
 
 func TestGetSumMiddlePageNumberFromRightUpdates(t *testing.T) {
 	tcs := []struct {
