35d5174f80ea34c57447740e874358282e2bc1ea
Oscar Motto Varea <oscar.motto@mail.schwarz>
2024-12-13 07:24:30 +0100

    feat: day13 solved by system linear equations

--- src/day13/main.go
+++ src/day13/main.go
@@ -2,14 +2,106 @@ package main
 
 import (
 	"fmt"
+	"math"
 	"path/filepath"
+	"strconv"
+	"strings"
 
 	"advent2024/pkg/file"
 )
+
+const MAX_INT = 9223372036854775807
+
+type Game struct {
+	buttonA [2]int
+	buttonB [2]int
+	price   [2]int
+}
+
+func parseLine(s string, idx int) (x int, y int) {
+	partsA := strings.Split(s[idx:], ", ")
+	x, _ = strconv.Atoi(partsA[0][2:])
+	y, _ = strconv.Atoi(partsA[1][2:])
+	return x, y
+}
+
+func parseInput(s []string) []Game {
+	games := make([]Game, (len(s)+1)/4)
+	for idx := 0; idx < len(s); idx += 4 {
+		aX, aY := parseLine(s[idx], 10)
+		bX, bY := parseLine(s[idx+1], 10)
+		pX, pY := parseLine(s[idx+2], 7)
+		games[idx/4] = Game{
+			buttonA: [2]int{aX, aY},
+			buttonB: [2]int{bX, bY},
+			price:   [2]int{pX, pY},
+		}
+	}
+	return games
+}
+
+func getPrice(game Game) int {
+	minimum := MAX_INT
+	for a := 0; a < 100; a++ {
+		for b := 0; b < 100; b++ {
+			x := game.buttonA[0]*a + game.buttonB[0]*b
+			y := game.buttonA[1]*a + game.buttonB[1]*b
+			if x == game.price[0] && y == game.price[1] && (a*3+b) < minimum {
+				minimum = a*3 + b
+			}
+		}
+	}
+	if minimum != MAX_INT {
+		return minimum
+	}
+	return 0
+}
+
+func getTokens(s []string) int {
+	games := parseInput(s)
+	result := 0
+	for _, game := range games {
+		result += getPrice(game)
+	}
+	return result
+}
+
+func getPriceByEquation(game Game, delta int) int {
+	/*
+		2x2 System Linear Equations
+		a * aX + b * bX = pX
+		a * aY + b * bY = pY
+	*/
+	pX := game.price[0] + delta
+	pY := game.price[1] + delta
+	aX := game.buttonA[0]
+	aY := game.buttonA[1]
+	bX := game.buttonB[0]
+	bY := game.buttonB[1]
+
+	a := float64(pX*bY-pY*bX) / float64(aX*bY-aY*bX)
+	b := float64(pY*aX-pX*aY) / float64(aX*bY-aY*bX)
+
+	// if there is no decimals is valid
+	if a == math.Trunc(a) && b == math.Trunc(b) {
+		return int(a*3 + b)
+	}
+	return 0
+}
+
+func getTokens2(s []string, delta int) int {
+	games := parseInput(s)
+	result := 0
+	for _, game := range games {
+		result += getPriceByEquation(game, delta)
+	}
+	return result
+}
 
 func main() {
 	absPathName, _ := filepath.Abs("src/day13/input.txt")
 	output, _ := file.ReadInput(absPathName)
 
-	fmt.Println(getPrices(output))
+	fmt.Println(getTokens(output))
+	fmt.Println(getTokens2(output, 10000000000000))
 }
--- src/day13/main_test.go
+++ src/day13/main_test.go
@@ -6,53 +6,75 @@ import (
 	"github.com/google/go-cmp/cmp"
 )
 
-func TestGetPrices(t *testing.T) {
+func TestGetTokens(t *testing.T) {
 	tcs := []struct {
 		desc   string
 		input  []string
 		result int
 	}{
 		{
-			desc: "In the first example, region A has price 4 * 10 = 40, region B has price 4 * 8 = 32, region C has price 4 * 10 = 40, region D has price 1 * 4 = 4, and region E has price 3 * 8 = 24. So, the total price for the first example is 140.",
+			desc: "So, the most prizes you could possibly win is two; the minimum tokens you would have to spend to win all (two) prizes is 480.",
 			input: []string{
-				"AAAA",
-				"BBCD",
-				"BBCC",
-				"EEEC",
+				"Button A: X+94, Y+34",
+				"Button B: X+22, Y+67",
+				"Prize: X=8400, Y=5400",
+				"",
+				"Button A: X+26, Y+66",
+				"Button B: X+67, Y+21",
+				"Prize: X=12748, Y=12176",
+				"",
+				"Button A: X+17, Y+86",
+				"Button B: X+84, Y+37",
+				"Prize: X=7870, Y=6450",
+				"",
+				"Button A: X+69, Y+23",
+				"Button B: X+27, Y+71",
+				"Prize: X=18641, Y=10279",
 			},
-			result: 140,
-		},
-		{
-			desc: "In the second example, the region with all of the O plants has price 21 * 36 = 756, and each of the four smaller X regions has price 1 * 4 = 4, for a total price of 772 (756 + 4 + 4 + 4 + 4).",
-			input: []string{
-				"OOOOO",
-				"OXOXO",
-				"OOOOO",
-				"OXOXO",
-				"OOOOO",
-			},
-			result: 772,
-		},
-		{
-			desc: "So, it has a total price of 1930",
-			input: []string{
-				"RRRRIICCFF",
-				"RRRRIICCCF",
-				"VVRRRCCFFF",
-				"VVRCCCJFFF",
-				"VVVVCJJCFE",
-				"VVIVCCJJEE",
-				"VVIIICJJEE",
-				"MIIIIIJJEE",
-				"MIIISIJEEE",
-				"MMMISSJEEE",
-			},
-			result: 1930,
+			result: 480,
 		},
 	}
 	for _, tc := range tcs {
 		t.Run(tc.desc, func(t *testing.T) {
-			got := getPrices(tc.input)
+			got := getTokens(tc.input)
+			if diff := cmp.Diff(tc.result, got); diff != "" {
+				t.Errorf("sum has diff %s", diff)
+			}
+		})
+	}
+}
+
+func TestGetTokens2(t *testing.T) {
+	tcs := []struct {
+		desc   string
+		input  []string
+		result int
+	}{
+		{
+			desc: "So, the most prizes you could possibly win is two; the minimum tokens you would have to spend to win all (two) prizes is 480.",
+			input: []string{
+				"Button A: X+94, Y+34",
+				"Button B: X+22, Y+67",
+				"Prize: X=8400, Y=5400",
+				"",
+				"Button A: X+26, Y+66",
+				"Button B: X+67, Y+21",
+				"Prize: X=12748, Y=12176",
+				"",
+				"Button A: X+17, Y+86",
+				"Button B: X+84, Y+37",
+				"Prize: X=7870, Y=6450",
+				"",
+				"Button A: X+69, Y+23",
+				"Button B: X+27, Y+71",
+				"Prize: X=18641, Y=10279",
+			},
+			result: 480,
+		},
+	}
+	for _, tc := range tcs {
+		t.Run(tc.desc, func(t *testing.T) {
+			got := getTokens2(tc.input, 0)
 			if diff := cmp.Diff(tc.result, got); diff != "" {
 				t.Errorf("sum has diff %s", diff)
 			}
